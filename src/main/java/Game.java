9ca1018ee360c331ba52c0b54bdea69671699c84
Pedro Veloso <pedroveloso@gmail.com>
2021-11-15 18:58:29 +0000

    Added moving X char

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,13 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
+
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game{
@@ -11,27 +15,58 @@ public class Game{
     private DefaultTerminalFactory terminalFactory;
     private Terminal terminal;
     private Screen screen;
-    
+
+    private int x = 10;
+    private int y = 10;
+
     public Game() throws IOException {
-            terminalSize = new TerminalSize(40, 20);
-            terminalFactory = new
+        terminalSize = new TerminalSize(40, 20);
+        terminalFactory = new
                     DefaultTerminalFactory()
                     .setInitialTerminalSize(terminalSize);
-            terminal = terminalFactory.createTerminal();
-            screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
+        terminal = terminalFactory.createTerminal();
+        screen = new TerminalScreen(terminal);
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
     }
 
     private void draw() throws IOException {
-            screen.clear();
-            screen.setCharacter(10, 10,
+        screen.clear();
+        screen.setCharacter(x, y,
                     TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
+        screen.refresh();
+    }
+    private void processKey(KeyStroke key){
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            default:
+        }
+        System.out.println(key);
     }
 
     public void run() throws IOException {
-        draw();
+        while(true){
+            draw();
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            if ((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                || key.getKeyType() == KeyType.EOF) {
+                screen.close();
+                break;
+            }
+        }
+
     }
 }
